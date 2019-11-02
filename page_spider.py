3963105296c75bad444be9889adb05e6c8e8bcbc
Eric Siegel <erics@ufl.edu>
2019-11-01 22:45:19 -0400

    finished url_utilities.py

--- page_spider.py
+++ page_spider.py
@@ -1,11 +1,13 @@
 import os
 import argparse
 
+
 def main(database: str, url_list_file: str):
     print("we are going to work with " + database)
     print("we are going to scan " + url_list_file)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-db", "--database", help="SQLite File Name")
     parser.add_argument("-i", "--input", help="File containing urls to read")
@@ -13,4 +15,3 @@ if __name__=='__main__':
     database_file = args.database
     input_file = args.input
     main(database=database_file, url_list_file=input_file)
-
